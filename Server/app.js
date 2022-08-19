429c1846bb34d6e8c41a595cf9879d3eba9a8e43
dhkim <rlawkdgks111@naver.com>
2022-08-19 09:28:02 +0900

    Setting init server

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const app = express();
 const routes = require("./routes");
+const yaml = require("yamljs");
+const swaggerUI = require("swagger-ui-express");
 
+const app = express();
+
+
+const openAPIDocument = yaml.load('./api/openapi.yaml');
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(openAPIDocument));
 app.use("/", routes);
 
 const PORT = process.env.PORT || 4000;
---
+++ Server/db/database.js
@@ -0,0 +1,10 @@
+import { config } from '../config.js';
+import SQ from 'sequelize';
+
+const { host, user, database, password } = config.db;
+export const sequelize = new SQ.Sequelize(database, user, password, {
+  host,
+  dialect: 'mysql',
+  logging: false,
+  timezone: "+09:00"
+});
